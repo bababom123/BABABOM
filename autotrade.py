a278ae24c7b8f58d449affdb8cd79c357e3afe1e
bababom123 <96180316+bababom123@users.noreply.github.com>
2022-03-31 22:33:27 +0900

    Update autotrade.py

--- autotrade.py
+++ autotrade.py
@@ -2,6 +2,12 @@ import pyupbit
 import datetime
 import time
 
+# with open("upbit_openapikey.txt") as f:
+#     lines = f.readlines()
+#     key = lines[0].strip()
+#     secret = lines[1].strip()
+#     upbit = pyupbit.Upbit(key, secret)
+
 upbit = pyupbit.Upbit("wk4aLyOF6apllUZqRZQS3iRxvxoPPzC9OVLl6KLY", "Fg5qbayqsq2dTxyeYCkpJZ3M7xBf2nLx0XoatSMj")
 
 KRW = upbit.get_balance("KRW") # 원화 보유액
@@ -9,7 +15,6 @@ KRW = upbit.get_balance("KRW") # 원화 보유액
 def get_target_price(ticker):
     df = pyupbit.get_ohlcv(ticker) # 시가, 고가, 저가, 종가, 거래량
     yesterday = df.iloc[-2] 
-
     today_open = yesterday['close']
     yesterday_high = yesterday['high']
     yesterday_low = yesterday['low']
@@ -32,15 +37,23 @@ def get_yesterday_ma_n(ticker, n):
     ma_n = close.rolling(window=n).mean()
     return ma_n[-2]
 
+def get_start_time(ticker):
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    start_time = df.index[0]
+    return start_time
+
 tickers = ['KRW-BTC', 'KRW-ETH', 'KRW-BCH', 'KRW-AAVE', 'KRW-LTC', 'KRW-SOL', 'KRW-BSV', 'KRW-AVAX', 'KRW-AXS', 'KRW-WAVES', 'KRW-ETC', 'KRW-BTG', 'KRW-STRK', 'KRW-ATOM', 'KRW-NEO', 'KRW-DOT', 'KRW-LINK', 'KRW-REP', 'KRW-NEAR', 'KRW-QTUM']
+# tickers = list(sc.get_Top_MarketGap_n(50)['ticker'])
 
 now = datetime.datetime.now()
-mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1) # 자정
 Basket = []
 
 print("start")
 while True:
     try:
+        now = datetime.datetime.now()
+        end_time = get_start_time(ticker) + datetime.timedelta(days=1) # 다음날 9시
+
         if not Basket: # 산 코인이 없다면
             for ticker in tickers:               
                 target_price = get_target_price(ticker)
@@ -52,18 +65,17 @@ while True:
                     buy_crypto_currency(ticker)
                     Basket.append(ticker)
                     print(ticker, " BUY")
-                time.sleep(0.3)
+                time.sleep(0.3)            
         else:
-            ticker = Basket[0]
-            now = datetime.datetime.now()
-            if mid < now < mid + datetime.timedelta(second=10): # 코드의 실행 속도 때문에 정확한 시간 비교는 불가능하므로 범위를 통해 유추한다.
+            ticker = Basket[0]           
+            if end_time - datetime.timedelta(second=5) < now < end_time: # 코드의 실행 속도 때문에 정확한 시간 비교는 불가능하므로 범위를 통해 유추한다.
                 now = datetime.datetime.now()
-                mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
                 ma5 = get_yesterday_ma_n(ticker, 5)
                 sell_crypto_currency(ticker)
                 Basket.pop()
                 print(ticker, " SELL")
-        print("whee...")
+            time.sleep(0.3)
+        
     except:
         print("ERR")
-        exit(1)    
+        time.sleep(1)
